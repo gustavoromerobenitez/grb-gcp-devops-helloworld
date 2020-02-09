6b9cbacb6997901215245bb9250e27205eaf1a1c
Gustavo Benitez <gusb79@gmail.com>
2020-02-09 20:17:20 +0000

    Changed Domain Name

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -303,9 +303,9 @@ resource "google_compute_global_address" "global-address" {
 #########################################################
 resource "google_dns_managed_zone" "dns-zone" {
   project = local.project-name
-  name = "${var.environment}-grb-gcp-devops-com"
-  dns_name = "${var.environment}.grb-gcp-devops.com."
-  description = "${var.environment} DNS zone"
+  name = "grbdevops-net"
+  dns_name = "grbdevops.net."
+  description = "DNS zone"
   labels = {
     terraform = "true"
   }
@@ -315,20 +315,11 @@ resource "google_dns_managed_zone" "dns-zone" {
 
 resource "google_dns_record_set" "dns-a-record-set" {
   project = local.project-name
-  name = "leonteq.${google_dns_managed_zone.dns-zone.dns_name}"
+  name = "leonteq-${var.environment}.${google_dns_managed_zone.dns-zone.dns_name}"
   managed_zone = google_dns_managed_zone.dns-zone.name
   type = "A"
   ttl = "300"
   rrdatas = [ google_compute_global_address.global-address.address ]
-  depends_on = [ google_project.current-project, google_project_service.dns-googleapis-com, google_project_service.compute-googleapis-com, google_dns_managed_zone.dns-zone, google_compute_global_address.global-address ]
-}
-
-resource "google_dns_record_set" "dns-cname-record-set" {
-  name         = "frontend.${google_dns_managed_zone.dns-zone.dns_name}"
-  managed_zone = google_dns_managed_zone.dns-zone.name
-  type         = "CNAME"
-  ttl          = "300"
-  rrdatas      = [ "leonteq.${google_dns_managed_zone.dns-zone.dns_name}" ]
   depends_on = [ google_project.current-project, google_project_service.dns-googleapis-com, google_project_service.compute-googleapis-com, google_dns_managed_zone.dns-zone, google_compute_global_address.global-address ]
 }
 

bd7553cee98d8f28fc3d9b4586cd1a5df3b32f76
Gustavo Benitez <gusb79@gmail.com>
2020-03-29 16:12:40 +0100

    Fixing errors

--- kubernetes/main.tf
+++ kubernetes/main.tf
@@ -209,15 +209,21 @@ resource "kubernetes_service_account" "k8s-service-account" {
 #  service_account = var.k8s-service-account-name # Description: Kubernetes service account to grant permissions to
 #}
 
-module "kubernetes-engine_workload-identity" {
-  source  = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
-  version = "7.3.0"
-  name = var.k8s-service-account-name
-  project_id = local.project-name
-  k8s_sa_name = var.k8s-service-account-name
-  use_existing_k8s_sa = true
-  namespace = "default"
-}
+#
+# Error: Error applying IAM policy for service account 'projects/k8s-tf-dev/serviceAccounts/k8s-service-account@k8s-tf-dev.iam.gserviceaccount.com': Error setting IAM policy for service account 'projects/k8s-tf-dev/serviceAccounts/k8s-service-account@k8s-tf-dev.iam.gserviceaccount.com': googleapi: Error 400: Identity namespace does not exist (k8s-tf-dev.svc.id.goog)., badRequest
+#
+#  on .terraform/modules/kubernetes-engine_workload-identity/terraform-google-kubernetes-engine-7.3.0/modules/workload-identity/main.tf line 61, in resource "google_service_account_iam_member" "main":
+#  61: resource "google_service_account_iam_member" "main" {
+#
+#module "kubernetes-engine_workload-identity" {
+#  source  = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
+#  version = "7.3.0"
+#  name = var.k8s-service-account-name
+#  project_id = local.project-name
+#  k8s_sa_name = var.k8s-service-account-name
+#  use_existing_k8s_sa = true
+#  namespace = "default"
+#}
 
 # TODO  Assign permissions to the created Google Service Account to allow K8s to access resources
 

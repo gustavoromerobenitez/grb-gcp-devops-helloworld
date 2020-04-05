46c0b8833041b1c2e50d5caa4c3f84a38038e2d5
Gustavo Benitez <gusb79@gmail.com>
2020-04-05 21:57:25 +0100

    Separating container-cluster creation

---
+++ container-cluster/backend.tf
@@ -0,0 +1,11 @@
+#
+# Terraform docs re: configuring back end: https://www.terraform.io/docs/backends/types/gcs.html
+#
+# Versioning has been enabled on the bucket gs://grb-gcp-devops-terraform
+# via the command gsutil versioning set on gs://grb-gcp-devops-terraform
+#
+terraform {
+  backend "gcs" {
+    
+  }
+}
---
+++ container-cluster/main.tf
@@ -0,0 +1,45 @@
+locals {
+  project-name="${var.project-base-name}-${var.environment}"
+  state-bucket-prefix="terraform/${local.project-name}/state/"
+}
+
+###############################################################################
+#
+# Providers
+#
+# Neither Variables nor Local references are allowed in the provider declaration
+# which prevents this file from being fully parameterized.
+#
+# This file should be templated to avoid the limitations imposed by Terraform
+###############################################################################
+
+
+data "terraform_remote_state" "current-project" {
+  backend = "gcs"
+  config = {
+    bucket = var.remote-state-bucket
+    prefix = local.state-bucket-prefix
+  }
+}
+
+provider "google" {
+  version = ">= 3.15"
+  region  = "europe-west2"
+  zone  = "europe-west2-a"
+  # enable_batching = "false"
+}
+
+provider "kubernetes" {
+  version = "~> 1.10"
+  host = google_container_cluster.container-cluster.endpoint
+  insecure = "false"
+  client_certificate = base64decode(google_container_cluster.container-cluster.master_auth.0.client_certificate)
+  client_key = base64decode(google_container_cluster.container-cluster.master_auth.0.client_key)
+  cluster_ca_certificate = base64decode(google_container_cluster.container-cluster.master_auth.0.cluster_ca_certificate)
+}
+
+resource "google_project_service" "container-googleapis-com" {
+  project = local.project-name
+  service = "container.googleapis.com"
+  disable_dependent_services = "true"
+}

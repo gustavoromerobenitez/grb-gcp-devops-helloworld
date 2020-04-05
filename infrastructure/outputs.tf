e4360ef491d1a1386c566afe16a4924412d16de5
Gustavo Benitez <gusb79@gmail.com>
2020-04-05 21:17:45 +0100

    Fixing outputs

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -2,30 +2,6 @@ output "google_project_current-project_number" {
   value = google_project.current-project.number
 }
 
-output "google_container_cluster_container-cluster_master_auth_0_client_certificate" {
-  value = google_container_cluster.container-cluster.master_auth.0.client_certificate
-}
-
-output "google_container_cluster_container-cluster_master_auth_0_client_key" {
-  value = google_container_cluster.container-cluster.master_auth.0.client_key
-}
-
-output "google_container_cluster_container-cluster_master_auth_0_cluster_ca_certificate" {
-  value = google_container_cluster.container-cluster.master_auth.0.cluster_ca_certificate
-}
-
-output "google_container_cluster_container-cluster_endpoint" {
-  value = google_container_cluster.container-cluster.endpoint
-}
-
-output "google_container_cluster_container-cluster_name" {
-  value = google_container_cluster.container-cluster.name
-}
-
-output "google_container_cluster_container-cluster_master_version" {
-  value = google_container_cluster.container-cluster.master_version
-}
-
 output "google_service_account_project-service-account_email" {
   value = google_service_account.project-service-account.email
 }
---
+++ kubernetes/outputs.tf
@@ -0,0 +1,23 @@
+output "google_container_cluster_container-cluster_master_auth_0_client_certificate" {
+  value = google_container_cluster.container-cluster.master_auth.0.client_certificate
+}
+
+output "google_container_cluster_container-cluster_master_auth_0_client_key" {
+  value = google_container_cluster.container-cluster.master_auth.0.client_key
+}
+
+output "google_container_cluster_container-cluster_master_auth_0_cluster_ca_certificate" {
+  value = google_container_cluster.container-cluster.master_auth.0.cluster_ca_certificate
+}
+
+output "google_container_cluster_container-cluster_endpoint" {
+  value = google_container_cluster.container-cluster.endpoint
+}
+
+output "google_container_cluster_container-cluster_name" {
+  value = google_container_cluster.container-cluster.name
+}
+
+output "google_container_cluster_container-cluster_master_version" {
+  value = google_container_cluster.container-cluster.master_version
+}
